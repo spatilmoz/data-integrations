dbb338bf894655f5302bae217f8f6064f6fa8420
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-08-25 23:55:04 -0400

    add scripts to installed list

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-
 from setuptools import setup, find_packages
+import os
 
 setup(name='data-integrations',
       version='0.0.1',
@@ -8,6 +8,7 @@ setup(name='data-integrations',
       author='Chris Valaas',
       author_email='cvalaas@mozilla.com',
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      scripts=[s for s in setuptools.findall('bin/') if os.path.splitext(s)[1] != '.pyc'],
       install_requires=[
         'requests',
       ]
