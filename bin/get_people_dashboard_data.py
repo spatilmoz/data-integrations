ef5190354aa402340acf09c700135e34cc04cf00
Chris Valaas <cvalaas@mozilla.com>
2018-09-27 11:35:28 -0700

    adding a monthly option

--- bin/get_people_dashboard_data.py
+++ bin/get_people_dashboard_data.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
   parser.add_argument('-d', '--debug', action='store', help='debug level', type=int, default=3)
   parser.add_argument('--date', action='store', help='date to retrieve', type=str)
   parser.add_argument('-o', '--output-dir', action='store', help='output directory', type=str, default='./')
-  #parser.add_argument('--output-filename', action='store', help='output filename', type=str)
   parser.add_argument('-f', '--force', action='store_true', help='run on a date even if it\'s not friday')
+  parser.add_argument('-m', '--monthly', action='store_true', help='pull the monthly version of the reports')
   args = parser.parse_args()
   
   debug = args.debug
@@ -32,9 +32,19 @@ if __name__ == "__main__":
 
   retrieve_date_l = [ int(i) for i in args.date.split('-') ]
   retrieve_date   = datetime.date(retrieve_date_l[0], retrieve_date_l[1], retrieve_date_l[2])
-  if retrieve_date.isoweekday() != 5 and not args.force:
-    print_debug(1, "Specified date (%s) is not a Friday. Use --force if you're sure" % retrieve_date)
-    exit()
+  start_date      = None
+  if args.monthly:
+    if retrieve_date_l[2] != 1 and not args.force:
+      print_debug(1, "Specified date (%s) is not the first of the month. Use --force if you're sure" % retrieve_date)
+      exit()
+    first_day  = retrieve_date.replace(day=1)
+    last_month = first_day - datetime.timedelta(days=1)
+    start_date = last_month.strftime("%Y-%m-01")
+
+  else:
+    if retrieve_date.isoweekday() != 5 and not args.force:
+      print_debug(1, "Specified date (%s) is not a Friday. Use --force if you're sure" % retrieve_date)
+      exit()
 
   if not os.path.isdir(args.output_dir):
     print_debug(1, "Specified output dir (%s) is not a directory." % args.output_dir)
@@ -53,7 +63,8 @@ if __name__ == "__main__":
 
     print_debug(3, "Getting %s data" % report_type)
     outfile = os.path.join(args.output_dir, report_type + '_' + str(retrieve_date) + '.csv')
-    wd_csv_data = Workday.get_dashboard_data(report_type, str(retrieve_date))
+
+    wd_csv_data = Workday.get_dashboard_data(report_type, str(retrieve_date), start_date)
 
     print_debug(3, "Writing %s data to %s" % (report_type, outfile))
 
