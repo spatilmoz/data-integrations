61d2ef1d7919de6912cb4c3b97f45136ab80e95e
jspiropulo-44926 <spiropulo@gmail.com>
2019-09-04 15:23:11 -0700

    DA-1198 testing the bigquery orchestrator.

--- integrations/api/connectors/gcp/bigquery_to_storage_process.py
+++ integrations/api/connectors/gcp/bigquery_to_storage_process.py
@@ -6,10 +6,9 @@ import integrations.api.utils.bigquery_client
 
 class BigQueryToStorageProcess(AbstractOrchestratorTask):
 
-    def __init__(self, bucket_name: str, dataset_id: str, project_id: str, file_extension: str, location: str):
+    def __init__(self, bucket_name: str, dataset_id: str, file_extension: str, location: str):
         self.bucket_name = bucket_name
         self.dataset_id = dataset_id
-        self.project_id = project_id
         self.file_extension = file_extension
         self.location = location
         self.logger = logging.getLogger(__name__)
@@ -24,7 +23,6 @@ class BigQueryToStorageProcess(AbstractOrchestratorTask):
                 self.logger.info("Found table: '{}".format(table.table_id))
                 self.logger.info("Exporting '{}' to bucket".format(table.table_id))
                 self.bq_client.extract_table(bucket_name=self.bucket_name,
-                                             project=self.project_id,
                                              dataset_id=self.dataset_id,
                                              table_id=table.table_id,
                                              file_extension=self.file_extension,
--- integrations/tests/unit/orchestrators/test_bigquery_to_storage_orchestrator.py
+++ integrations/tests/unit/orchestrators/test_bigquery_to_storage_orchestrator.py
@@ -1,25 +1,29 @@
 import unittest
-from integrations.api.connectors.gcp.bigquery_to_storage_process import BigQueryToStorageProcess
 from unittest.mock import Mock
 from unittest import mock
-
+from integrations.api.orchestrators.impl.bigquery_to_storage_orchestrator import BigQueryToStorageOrchestrator
 from integrations.tests.unit.mocks.bigquery_client_mock import BigqueryClientMock
 
 
-class TestBigQueryToStorageProcess(unittest.TestCase):
+class TestBigQueryToStorageOrchestrator(unittest.TestCase):
+
     @mock.patch('integrations.api.utils.bigquery_client')
     def test_orchestration_without_tables(self, mocked_instance):
         mocked_instance.BigQueryClient.return_value = Mock(return_value=BigqueryClientMock())()
-        _test = BigQueryToStorageProcess('bucket1', 'dataset_id1', 'project_id1' , 'csv', 'US')
-        _test.execute()
+        under_test = BigQueryToStorageOrchestrator(bucket='dp2-dev-data-to-salesforce', dataset='cdp_to_salesforce',
+                                      file_extension='csv', location='US')
+
+        under_test.orchestrate()
+
 
     @mock.patch('integrations.api.utils.bigquery_client')
     def test_orchestration_with_tables(self, mocked_instance):
         tables = [Mock()]
         mocked_instance.BigQueryClient.return_value = Mock(return_value=BigqueryClientMock(tables))()
-        _test = BigQueryToStorageProcess('bucket1', 'dataset_id1', 'project_id1', 'csv', 'US')
-        _test.execute()
+        under_test = BigQueryToStorageOrchestrator(bucket='dp2-dev-data-to-salesforce', dataset='cdp_to_salesforce',
+                                      file_extension='csv', location='US')
+
+        under_test.orchestrate()
 
 
-if __name__ == "__main__":
-   unittest.main()
+
