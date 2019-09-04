5f0e83d31617a14b68963eb2605241522b98bc7a
jspiropulo-44926 <spiropulo@gmail.com>
2019-09-04 16:27:58 -0700

    Update test_bigquery_to_storage_process.py
    removing project parameter.

--- integrations/tests/unit/connectors/gcp/test_bigquery_to_storage_process.py
+++ integrations/tests/unit/connectors/gcp/test_bigquery_to_storage_process.py
@@ -10,14 +10,14 @@ class TestBigQueryToStorageProcess(unittest.TestCase):
     @mock.patch('integrations.api.utils.bigquery_client')
     def test_orchestration_without_tables(self, mocked_instance):
         mocked_instance.BigQueryClient.return_value = Mock(return_value=BigqueryClientMock())()
-        _test = BigQueryToStorageProcess('bucket1', 'dataset_id1', 'project_id1' , 'csv', 'US')
+        _test = BigQueryToStorageProcess('bucket1', 'dataset_id1' , 'csv', 'US')
         _test.execute()
 
     @mock.patch('integrations.api.utils.bigquery_client')
     def test_orchestration_with_tables(self, mocked_instance):
         tables = [Mock()]
         mocked_instance.BigQueryClient.return_value = Mock(return_value=BigqueryClientMock(tables))()
-        _test = BigQueryToStorageProcess('bucket1', 'dataset_id1', 'project_id1', 'csv', 'US')
+        _test = BigQueryToStorageProcess('bucket1', 'dataset_id1', 'csv', 'US')
         _test.execute()
 
 
