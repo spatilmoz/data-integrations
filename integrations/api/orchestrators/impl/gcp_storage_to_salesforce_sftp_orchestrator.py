022eaaf70b509fbf459510965ebc8d302aa0e7a4
jspiropulo-44926 <spiropulo@gmail.com>
2019-09-04 17:01:27 -0700

    Covering orchestration for storage to salesforce
    testing ochestration without touching implementation of the tasks.

--- integrations/api/orchestrators/impl/gcp_storage_to_salesforce_sftp_orchestrator.py
+++ integrations/api/orchestrators/impl/gcp_storage_to_salesforce_sftp_orchestrator.py
@@ -1,8 +1,8 @@
-from integrations.api.connectors.SalesforceFTP.salesforce_sftp_connector import SalesforceSftpConnector
-from integrations.api.connectors.gcp.gcp_storage_connector import GcpStorageConnector
+import integrations.api.connectors.SalesforceFTP.salesforce_sftp_connector
+import integrations.api.connectors.gcp.gcp_storage_connector
 from integrations.api.orchestrators.abstract_orchestrator import AbstractOrchestrator
 from integrations.api.orchestrators.orchestrator_executor import OrchestratorExecutor
-from integrations.api.transformers.encryptor_transformer import EncryptorTransformer
+import integrations.api.transformers.encryptor_transformer
 
 
 class GcpStorageToSalesforceSftpOrchestrator(AbstractOrchestrator):
@@ -14,5 +14,9 @@ class GcpStorageToSalesforceSftpOrchestrator(AbstractOrchestrator):
         self.destination_dir = destination_dir
 
     def orchestrate(self):
-        nodes = [GcpStorageConnector(self.bucket, self.dataset), EncryptorTransformer(self.source_dir), SalesforceSftpConnector(self.source_dir, self.destination_dir)]
+        storage_connector = integrations.api.connectors.gcp.gcp_storage_connector.GcpStorageConnector(self.bucket, self.dataset)
+        transformer = integrations.api.transformers.encryptor_transformer.EncryptorTransformer(self.source_dir)
+        sftp_connector = integrations.api.connectors.SalesforceFTP.salesforce_sftp_connector.SalesforceSftpConnector(self.source_dir, self.destination_dir)
+
+        nodes = [storage_connector, transformer, sftp_connector]
         OrchestratorExecutor.execute(nodes)
--- integrations/tests/unit/orchestrators/test_gcp_storage_to_salesforce_sftp_orchestrator.py
+++ integrations/tests/unit/orchestrators/test_gcp_storage_to_salesforce_sftp_orchestrator.py
@@ -7,27 +7,30 @@ from integrations.api.orchestrators.impl.gcp_storage_to_salesforce_sftp_orchestr
 
 
 class GcpStorageConnectorMock():
-    pass
+    def __call__(self, *args, **kwargs):
+        pass
+
 
 class EncryptorTransformerMock():
     pass
+
 
 class SalesforceSftpConnectorMock():
     pass
 
 
-class GcpStorageToSalesforceSftpOrchestratorTest(unittest.TestCase):
+class TestGcpStorageToSalesforceSftpOrchestrator(unittest.TestCase):
     pass
 
-    # @mock.patch('integrations.api.connectors.gcp.gcp_storage_connector')
-    # @mock.patch('integrations.api.transformers.encryptor_transformer')
-    # @mock.patch('integrations.api.connectors.SalesforceFTP.salesforce_sftp_connector')
-    # def test_mocked_orchestrator(self, mocked_storage, mocked_transformer, mocked_sftp):
-    #     mocked_storage.GcpStorageConnector.return_value = Mock(return_value=GcpStorageConnectorMock())()
-    #     mocked_transformer.EncryptorTransformer.return_value = Mock(return_value=EncryptorTransformerMock())
-    #     mocked_sftp.SalesforceSftpConnector.return_value = Mock(return_value=SalesforceSftpConnectorMock())()
-    #     under_test = GcpStorageToSalesforceSftpOrchestrator('dp2-dev-data-to-salesforce', 'cdp_to_salesforce')
-    #     under_test.orchestrate()
+    @mock.patch('integrations.api.connectors.gcp.gcp_storage_connector')
+    @mock.patch('integrations.api.transformers.encryptor_transformer')
+    @mock.patch('integrations.api.connectors.SalesforceFTP.salesforce_sftp_connector')
+    def test_mocked_orchestrator(self, mocked_storage, mocked_transformer, mocked_sftp):
+        mocked_storage.GcpStorageConnector.return_value = Mock(return_value=GcpStorageConnectorMock())
+        mocked_transformer.EncryptorTransformer.return_value = Mock(return_value=EncryptorTransformerMock())
+        mocked_sftp.SalesforceSftpConnector.return_value = Mock(return_value=SalesforceSftpConnectorMock())
+        under_test = GcpStorageToSalesforceSftpOrchestrator('dp2-dev-data-to-salesforce', 'cdp_to_salesforce', '', '')
+        under_test.orchestrate()
 
 
 if __name__ == "__main__":
