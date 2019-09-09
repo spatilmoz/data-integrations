eb54435f287f320d46140b152f83f71e7452ac46
marcovarrubias <mcovarrubias@mozilla.com>
2019-09-09 08:09:05 -0700

    Unblocking integration for salesforce

--- integrations/api/orchestrators/orchestrator_selector.py
+++ integrations/api/orchestrators/orchestrator_selector.py
@@ -1,6 +1,6 @@
 from integrations.api.orchestrators.impl.bigquery_to_storage_orchestrator import BigQueryToStorageOrchestrator
 from integrations.api.orchestrators.impl.gcp_storage_to_salesforce_sftp_orchestrator import GcpStorageToSalesforceSftpOrchestrator
-#from integrations.api.orchestrators.impl.workday_to_anaplan_orchestrator import WorkdayToAnaplanOrchestrator
+from integrations.api.orchestrators.impl.workday_to_anaplan_orchestrator import WorkdayToAnaplanOrchestrator
 from integrations.api.orchestrators.impl.gcp_cleanup_orchestrator import GcpCleanupOrchestrator
 from integrations.api.utils.input_args_worker import InputArgsWorker
 
@@ -10,10 +10,10 @@ class OrchestratorSelector:
         self.worker = InputArgsWorker(input_args)
 
     def delegate_orchestration(self):
-        #if 'financial_workday_to_anaplan' == self.worker.get_value('pipeline'):
-        #    WorkdayToAnaplanOrchestrator().orchestrate()
+        if 'financial_workday_to_anaplan' == self.worker.get_value('pipeline'):
+            WorkdayToAnaplanOrchestrator().orchestrate()
 
-        if 'marketing_storage_to_salesforce' == self.worker.get_value('pipeline'):
+        elif 'marketing_storage_to_salesforce' == self.worker.get_value('pipeline'):
             GcpStorageToSalesforceSftpOrchestrator(self.worker.get_value('bucket'),
                                                    self.worker.get_value('dataset'),
                                                    self.worker.get_value('source_dir'),
